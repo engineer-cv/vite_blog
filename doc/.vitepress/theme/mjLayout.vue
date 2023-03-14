79ae3cb804af4eca783f6ea44ddce6fd583d5c0f
wangming <894056459@qq.com>
2023-03-15 03:48:42 +0800

    版本更新

--- doc/.vitepress/theme/mjLayout.vue
+++ doc/.vitepress/theme/mjLayout.vue
@@ -10,11 +10,11 @@ onMounted(() => {
     document.querySelector(".content-container").appendChild(s_div); // querySelector的节点可自己根据自己想加载的地方设置
     var gitment = new Gitalk({
       id: location.pathname, // 可选。默认为 location.href
-      owner: "mjjh1717", // GitHub repository 所有者
-      repo: "blog", // GitHub repository
-      clientID: "a8e272eca5e30de72036", // 自己的clientID
-      clientSecret: "00301f0aee3291294890432216c7372d9fe99ce2", // 自己的clientSecret
-      admin: ["mjjh1717"], // GitHub repository 所有者
+      owner: "engineer-cv", // GitHub repository 所有者
+      repo: "vite_blog", // GitHub repository
+      clientID: "de0e2b49db65295e67e8", // 自己的clientID
+      clientSecret: "8dd6a716626b90a33aa8985c8949b88e8d0a650c", // 自己的clientSecret
+      admin: ["engineer-cv"], // GitHub repository 所有者
       labels: ["Gitalk"], // GitHub issue 的标签
       createIssueManually: true, //如果当前页面没有相应的 isssue 且登录的用户属于 admin，则会自动创建 issue。如果设置为 true，则显示一个初始化页面，创建 issue 需要点击 init 按钮。
     });
